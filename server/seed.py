5b1e39731456a14989bb0be825d613e712fd11a9
GeorgeKahugu <gee.kahugu@gmail.com>
2024-07-10 11:52:34 +0300

    created seed.py file, added imports and started building

--- server/seed.py
+++ server/seed.py
@@ -2,3 +2,19 @@ from models import db, Applicant, Job, Application
 from app import app
 from datetime import datetime 
 from faker import Faker
+
+def seed_data():
+    fake = Faker()
+
+    with app.app_context():
+
+    #Drop all tables and create them
+
+        db.drop_all()
+        db.create_all()
+    #Seed Users
+    
+          
+        
+        
+        
