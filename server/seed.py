f7e9cea2b96781eac1cfec17ce245ff5b44feae6
GeorgeKahugu <gee.kahugu@gmail.com>
2024-07-10 12:25:07 +0300

    added the seed users and seed jobs in seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,4 +1,4 @@
-from models import db, Applicant, Job, Application
+from models import User, db, Job, Application
 from app import app
 from datetime import datetime 
 from faker import Faker
@@ -12,8 +12,37 @@ def seed_data():
 
         db.drop_all()
         db.create_all()
-    #Seed Users
-    
+   
+         # Seed Users
+        users = []
+        for _ in range(5):
+            user = User(
+                username=fake.user_name(),
+                email=fake.email(),
+                password=generate_password_hash('password').decode('utf-8'),
+                role='employer'
+            )
+        
+        users.append(user)
+    db.session.add_all(users)
+    db.session.commit()
+
+    #Seed Jobs
+    jobs = []
+    for _ in range(10):
+        job = Job(
+                title=fake.job(),
+                description=fake.text(),
+                company=fake.company(),
+                location=fake.city(),
+                employer_id=fake.random_element(elements=[user.id for user in users if user.role == 'employer'])
+            )
+        
+        jobs.append(job)
+        db.session.add_all(jobs)
+        db.session.commit()   
+
+
           
         
         
