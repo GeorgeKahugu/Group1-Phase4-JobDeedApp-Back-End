82bda54fd2c63d7c04a3b2865e0cab9a3afbb98a
GeorgeKahugu <gee.kahugu@gmail.com>
2024-07-12 22:03:09 +0300

    added CRUD operations and routes to my Applicants in app.py

--- server/models.py
+++ server/models.py
@@ -1,11 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-class Applicant(db.Model):
+class Applicant(db.Model, SerializerMixin):
     __tablename__ = 'applicants'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -15,14 +16,14 @@ class Applicant(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now())
     role = db.Column(db.String(20),nullable=False)
 
-    job = db.relationship('Job', backref='applicants', lazy=True)
+    jobs = db.relationship('Job', back_populates='applicant')
     # applications = db.relationship('Application', backref='applicant', lazy=True)
 
 def __repr__(self):
     return f"<Applicant {self.id}: {self.username}>"
 
 
-class Job(db.Model):
+class Job(db.Model, SerializerMixin):
     __tablename__ = 'jobs'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -33,8 +34,23 @@ class Job(db.Model):
     employer_id=db.Column(db.Integer, db.ForeignKey('applicants.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
 
+    applicant = db.relationship('Applicant', back_populates='jobs')
+
 def __repr__(self):
     return f"<Job {self.id}: {self.title}>"
 
 
 
+class Applications(db.Model):
+    __tablename__ = 'applications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    job_id=db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+    status=db.Column(db.String(20), nullable=False)
+    date_applied=db.Column(db.DateTime, nullable=False)
+
+#  jobs = db.relationship('Job', back_populates='applicants')
+
+def __repr__(self):
+    return f"Applications {self.id}: {self.user_id}>"
