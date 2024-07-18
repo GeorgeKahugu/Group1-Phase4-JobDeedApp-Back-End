50f4f44e311526a9c91c4e22e2cedb5bcac92c8c
GeorgeKahugu <gee.kahugu@gmail.com>
2024-07-18 21:57:17 +0300

    worked on the README

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token
 from flask_cors import CORS
 
 from datetime import datetime
